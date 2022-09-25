ce72acf1e8d375f27f67321de12af8d4406e0303
Tucno0 <yantucnoperez@gmail.com>
2022-09-25 11:07:02 -0500

    fina

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 
 import { HeroesModule } from './heroes/heroes.module';
 import { ContadorModule } from './contador/contador.module';
+import { DbzModule } from './dbz/dbz.module';
 
 // Tarea: Crear un modulo llamado ContadorModule, Hacer las declaraciones y exportaciones
 
@@ -15,7 +15,8 @@ import { ContadorModule } from './contador/contador.module';
   imports: [
     BrowserModule,
     HeroesModule,
-    ContadorModule
+    ContadorModule,
+    DbzModule
   ],
   providers: [],
   bootstrap: [AppComponent]
